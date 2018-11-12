4c6061cf86f8069a9094100cd748d07b46197138
AmirDaoud22 <43315646+AmirDaoud22@users.noreply.github.com>
2018-11-12 18:52:01 +0200

    bbb

--- eatwell/index.php
+++ eatwell/index.php
@@ -40,7 +40,7 @@ type="image/png">
 
     <nav class="navbar navbar-expand-lg navbar-dark site_navbar bg-dark site-navbar-light" id="site-navbar">
       <div class="container">
-        <a class="navbar-brand" href="index.html">Ezba</a>
+        <a class="navbar-brand" href="index.php">Ezba</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#site-nav" aria-controls="site-nav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="oi oi-menu"></span> Menu
         </button>
